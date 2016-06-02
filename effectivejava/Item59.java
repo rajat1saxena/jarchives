3c53ffcf4b82181a232a163fecd8c5eb56c8f5f3
Rajat Saxena <rajat@raynstudios.in>
2016-06-02 09:53:49 +0530

    Item 59 added to effectivejava

---
+++ effectivejava/Item59.java
@@ -0,0 +1,6 @@
+/**
+* Representative of Item 59: Avoid unnecessary use of checked exceptions
+*
+* Again this item is mostly theoritical and I cannot think of a valid
+* testcase for this. So, no code is written for this item.
+*/
