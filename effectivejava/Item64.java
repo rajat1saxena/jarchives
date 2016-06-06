74efbb19825e2d76ba22d2c02af27222212b9ad3
Rajat Saxena <rajat@raynstudios.in>
2016-06-06 17:26:14 +0530

    Item 64 & 65 added to effectivejava

---
+++ effectivejava/Item64.java
@@ -0,0 +1,10 @@
+/**
+* Representative of Item 64: Strive for failure atomicity.
+*
+* This item basically suggest that the failed method invocation
+* should leave the object in the state that it was in prior to the
+* invocation.
+* 
+* TODO: I cannot think of any practical example in this case, except 
+* the one already provided in the book so I will leave it at that.
+*/
---
+++ effectivejava/Item65.java
@@ -0,0 +1,6 @@
+/**
+* Representative of Item 65: Don't ignore exceptions.
+*
+* Again, this is general best practise, reminded yet another time by
+* the author. No code is designed due to the same reason.
+*/
