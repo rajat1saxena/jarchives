d127c23d75f1592509ba78b92e54b2abd153e878
Rajat Saxena <rajat@raynstudios.in>
2016-06-10 15:26:29 +0530

    Commands enum example is enhanced further

--- enums/Commands.java
+++ enums/Commands.java
@@ -5,7 +5,23 @@
 */
 public class Commands {
 	enum Command {
-		GO, STOP, FORWARD, BACK, DIE
+		GO("goes"), 
+		STOP("stops"), 
+		FORWARD("forwards"), 
+		BACK("backs"), 
+		DIE("dies");
+		
+		private String commandString;
+		
+		// Enum constructors are always private
+		Command(String commandString) {
+			this.commandString = commandString;
+		}
+
+		@Override
+		public String toString() {
+			return commandString;
+		}
 	}
 
 	/**
@@ -19,23 +35,38 @@ public class Commands {
 			String result = name;
 			switch (commandword) {
 				case GO:
-					result = name + " goes";
+					result = name + " " + commandword;
 					break;
 				case STOP:
-					result = name + " stops";
+					result = name + " " + commandword;
 					break;
 				case FORWARD:
-					result = name + " forwards";
+					result = name + " " + commandword;
 					break;
 				case BACK:
-					result = name + " backs";
+					result = name + " " + commandword;
 					break;
 				case DIE:
-					result = name + " dies";
+					result = name + " " + commandword;
 					break;
 			}	
 			// Print out the result
 			System.out.println(result);
+			
+			// Enum values are objects, not ints
+			if (Command.GO instanceof Object) {
+				System.out.println("Go is an instance of Object");
+			}
+
+			// Enums inherit from Java standard class 'Enum'
+			//
+			// The class has three important methods i.e name(),
+			// ordinal() and the static method values().
+			Command cmd = Command.DIE;
+			System.out.format("Name, Ordinal: %s, %d\n", cmd.name(),cmd.ordinal());
+			for (Command c:Command.values()) {
+				System.out.println(c);
+			}
 		} catch (ArrayIndexOutOfBoundsException e) {
 			// e.printStackTrace();
 			System.out.println("Error! Supply player's name");
